6b15aa26981af63ae6aeb89979b4b41ae54f0a95
gsjoy24 <gour.joy24@gmail.com>
2024-01-14 23:57:07 +0600

    added review delete route

--- index.js
+++ index.js
@@ -251,6 +251,14 @@ async function run() {
 			res.send(result);
 		});
 
+		// delete a review
+		app.delete('/reviews/:id', async (req, res) => {
+			const id = req.params.id;
+			const query = { _id: new ObjectId(id) };
+			const result = await reviewCollection.deleteOne(query);
+			res.send(result);
+		});
+
 		// Send a ping to confirm a successful connection
 		await client.db('admin').command({ ping: 1 });
 		console.log('Pinged your deployment. You successfully connected to MongoDB!');
