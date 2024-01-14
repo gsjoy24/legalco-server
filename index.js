fbd48601243baa0480ddbc3e4d83b8185604aeb0
gsjoy24 <gour.joy24@gmail.com>
2024-01-14 23:55:41 +0600

    added review add route

--- index.js
+++ index.js
@@ -229,6 +229,13 @@ async function run() {
 			res.send(result);
 		});
 
+		// ! for review
+		app.post('/reviews', async (req, res) => {
+			const review = req.body;
+			const result = await reviewCollection.insertOne(review);
+			res.send(result);
+		});
+
 		// Send a ping to confirm a successful connection
 		await client.db('admin').command({ ping: 1 });
 		console.log('Pinged your deployment. You successfully connected to MongoDB!');
